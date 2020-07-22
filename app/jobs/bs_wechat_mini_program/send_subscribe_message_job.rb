8fb5c505a5eba7d36ee6ac996b75aa21b67420d1
MC <mc@beansmile.com>
2020-07-22 15:21:11 +0800

    fix: 修复发送模板消息问题

--- app/jobs/bs_wechat_mini_program/send_subscribe_message_job.rb
+++ app/jobs/bs_wechat_mini_program/send_subscribe_message_job.rb
@@ -3,7 +3,7 @@
 module BsWechatMiniProgram
   class SendSubscribeMessageJob < ApplicationJob
     def perform(appid, options)
-      response = BsWechatMiniProgram::Client.find_by_appid(appid).send_subscribe_message(options[:openid], options[:template_id], options[:data], { page: options[:page] })
+      response = BsWechatMiniProgram::Client.find_by_appid(appid).send_subscribe_message(options[:touser], options[:template_id], options[:data], { page: options[:page] })
 
       # 0代表成功
       # 43101代表用户拒收该订阅消息
