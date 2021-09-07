df8389483aaa21a39dfe5a2343601c7cab1d0f7f
MC <mc@beansmile.com>
2021-09-07 13:59:06 +0800

    fix: 修复code_to_session方法参数问题

--- lib/bs_wechat_mini_program/api/auth.rb
+++ lib/bs_wechat_mini_program/api/auth.rb
@@ -5,7 +5,7 @@ module BsWechatMiniProgram
     module Auth
       # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html
       # 登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程
-      def code_to_session(code: code)
+      def code_to_session(code:)
         body = {
           appid: appid,
           secret: secret,
