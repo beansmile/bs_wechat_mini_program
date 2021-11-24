93c7d2c296a6815584f5b063a3bedf4fad705a6b
MC <mc@beansmile.com>
2021-11-24 16:08:04 +0800

    feat: Add msg_sec_check API

--- lib/bs_wechat_mini_program/api/security.rb
+++ lib/bs_wechat_mini_program/api/security.rb
@@ -12,6 +12,19 @@ module BsWechatMiniProgram
 
         http_post("/wxa/img_sec_check", body: body)
       end
+
+      # https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/sec-check/security.msgSecCheck.html
+      # 检查一段文本是否含有违法违规内容。
+      def msg_sec_check(content:, scene:, openid:)
+        body = {
+          version: 2,
+          content: content,
+          openid: openid,
+          scene: scene
+        }
+
+        http_post("/wxa/msg_sec_check", body: body)
+      end
     end
   end
 end
